66e0eaca238f2325d79855ec4b3f48ffdd7b0918
DangNgocMinh <dangngocminh88@gmail.com>
2021-03-01 10:35:17 +0700

    Initial commit

---
+++ Entities/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    [Table("Course")]
+    public class Course
+    {
+        public long CourseId { get; set; }
+        public string CourseName { get; set; }
+        public string UdemyLink { get; set; }
+        public string ImageLink { get; set; }
+        public string Summary { get; set; }
+        public string Content { get; set; }
+    }
+}
